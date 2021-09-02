f6fc0c6da7cbcaa8f0f6e41735a4c49adee2d971
thibaudk <thibaud.keller@aquilenet.fr>
2021-09-02 22:54:55 +0200

    add vulkan-headers

--- bash/setup.sh
+++ bash/setup.sh
@@ -4,7 +4,7 @@ update()
 {
 pacman-key --init
 pacman-key --populate archlinuxarm
-pacman -Syu realtime-privileges git gcc clang pkgconfig cmake make ninja boost llvm qt5-base qt5-imageformats qt5-websockets qt5-quickcontrols2 qt5-serialport qt5-declarative qt5-tools qt5-svg qt5-webengine python-pip portaudio ffmpeg lv2 suil lilv sdl2 fftw --noconfirm
+pacman -Syu realtime-privileges git gcc clang pkgconfig cmake make ninja boost llvm qt5-base qt5-imageformats qt5-websockets qt5-quickcontrols2 qt5-serialport qt5-declarative qt5-tools qt5-svg qt5-webengine python-pip portaudio ffmpeg lv2 suil lilv sdl2 fftw vulkan-headers --noconfirm
 }
 
 user()
